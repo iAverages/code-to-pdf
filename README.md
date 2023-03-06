5963c0da282752af7c4e207b17445a7492a0f28a
iAverages <me@danielraybone.com>
2023-03-06 10:45:02 +0000

    done + cleanup

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # code-to-pdf
 
 Converts all files within a directory and places file contents within a single PDF.
+
+> This will probably never get updated or features added. I needed this for a college exam.
--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,90 @@
 import PDFDocument from "pdfkit";
 import fs from "fs";
 import inquirer from "inquirer";
-import glob from "glob";
+import ora, { Ora } from "ora";
+import { globby } from "globby";
 import { promisify } from "util";
 import { resolve } from "path";
-const globp = promisify(glob);
 const getFile = promisify(fs.readFile);
+const loader = ora();
 
 type Questions = {
     codePath: string;
+    fileName: string;
 };
 
-const getIgnoreFiles = async () => (await getFile(resolve(__dirname, "..", ".gitignore"))).toString();
+const __dirname = new URL(".", import.meta.url).pathname.replace("/", "");
 
-const getFiles = async (path: string) => globp(path /*{ ignore: await getIgnoreFiles() }*/);
+const getIgnoreFiles = async () => {
+    return (await getFile(resolve(__dirname, "..", ".gitignore"))).toString();
+};
+
+const getFiles = async (path: string) =>
+    // Not sure why these files dont work with the gitignore format
+    globby(["**/*", "!yarn.lock", "!*.ico", "!bin/**/*", "!obj/**/*", "!*.dll", "!debug/**/*"], {
+        cwd: path,
+        ignoreFiles: await getIgnoreFiles(),
+        gitignore: true,
+    });
+
+const ensureEndsWith = (end: string, str: string) => (str.endsWith(end) ? str : str + end);
+
+const getInput = async () => {
+    const { codePath, ...rest } = await inquirer.prompt<Questions>([
+        {
+            type: "input",
+            name: "codePath",
+            message: "Enter the full path to the code to pdfify",
+        },
+        {
+            type: "input",
+            name: "fileName",
+            message: "Enter the name of the output document",
+        },
+    ]);
+
+    return { ...rest, codePath: ensureEndsWith("/", codePath.replaceAll("\\", "/")) };
+};
+
+type AddTextPdf = {
+    pdf: PDFKit.PDFDocument;
+    loader: Ora;
+    files: string[];
+    basePath: string;
+};
+
+const addTextPdf = ({ basePath, files, loader, pdf }: AddTextPdf) => {
+    loader.start("Starting");
+    for (const filePath of files) {
+        loader.info(`Placing ${filePath} into PDF`);
+        const header = `\n\n\nSTART ${filePath}\n\n\n`;
+        const footer = `\n\n\nEND ${filePath}`;
+        const contents = fs
+            .readFileSync(basePath + filePath, { encoding: "utf8" })
+            .toString()
+            // Encoding issue I cba to figure out
+            // https://github.com/foliojs/pdfkit/issues/606#issuecomment-273312814
+            .replaceAll(/\r\n|\r/g, "\n");
+
+        pdf.text(header + contents + footer);
+    }
+};
 
 (async () => {
-    // const { codePath } = await inquirer.prompt<Questions>({
-    //     type: "input",
-    //     name: "codePath",
-    //     message: "Enter the full path to the code to pdfify",
-    // });
-    const codePath = "C:\\Users\\297882\\Documents\\college-2022-23\\projects\\os-mock-1\\**\\*.*";
-    const doc = new PDFDocument();
+    const { fileName, codePath } = await getInput();
 
-    doc.pipe(fs.createWriteStream("output.pdf"));
+    const doc = new PDFDocument();
+    doc.pipe(fs.createWriteStream(fileName));
 
     const files = await getFiles(codePath);
-    console.log(codePath);
-    console.log(files);
-    // doc.text(data.toString());
+
+    addTextPdf({
+        files,
+        loader,
+        pdf: doc,
+        basePath: codePath,
+    });
 
     doc.end();
+    loader.stop();
 })();
